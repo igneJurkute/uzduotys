39ae65b14fb5018fd59a0646d75ba1f1eda7553a
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-04-02 13:02:49 +0300

    uzduotys

---
+++ index.js
@@ -0,0 +1,27 @@
+//Kintamųjų inicijavimas
+console.log(1 + ' uzduotis');
+const num1 = 5;
+console.log(num1);
+const num2 = 17;
+console.log(num2);
+const num3 = 28;
+console.log(num3);
+
+console.log(2 + ' uzduotis');
+const word1 = 'Dangus';
+console.log(word1);
+const word2 = 'aitvaras';
+console.log(word2);
+const word3 = 'televizorius';
+console.log(word3);
+
+//Veiksmai su kintamaisiais
+console.log(1 + ' uzduotis');
+const sumNum = num1 + num2 + num3;
+console.log(sumNum);
+
+console.log(2 + ' uzduotis');
+const sumWord = word1 + ' ' + word2 + ' ' + word3;
+console.log(sumWord);
+
+
