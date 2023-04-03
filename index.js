bea2b634843d3e10651d8589f5b81be9285de131
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-04-04 00:10:30 +0300

    arrays

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ console.log(word2);
 const word3 = 'televizorius';
 console.log(word3);
 
+console.log(3 + ' uzduotis');
+const nums1 = [1, 20, 5, 9, 48];
+console.log(nums1);
+const nums2 = [2, 7, 10, 15, 25];
+console.log(nums2);
+const nums3 = [77, 66, 55, 44, 33];
+console.log(nums3);
+
+console.log(4 + ' uzduotis');
+const names = ['Loreta', 'Jonas', 'Greta', 'Nedas', 'Karina'];
+console.log(names);
+const colors = ['red', 'green', 'blue', 'orange', 'white'];
+console.log(colors);
+const things = ['house', 'tree', 'sun', 'ball', 'lamp'];
+console.log(things);
+
+console.log('------------------------');
 //Veiksmai su kintamaisiais
 console.log(1 + ' uzduotis');
 const sumNum = num1 + num2 + num3;
@@ -23,5 +40,11 @@ console.log(sumNum);
 console.log(2 + ' uzduotis');
 const sumWord = word1 + ' ' + word2 + ' ' + word3;
 console.log(sumWord);
+
+console.log(4 + ' uzduotis');
+const sumArray = names.join(', ') + ', ' + colors.join(', ') + ', ' + things.join(', ');
+console.log(sumArray);
+const sumArray2 = names.concat(colors, things);
+console.log(sumArray2);
 
 
