0cf0fa2a8c6e8c7ff18fb184e17768d14dd917ce
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-04-06 11:09:24 +0300

    functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+console.clear();
+
+console.log('Funkcijos');
+
+console.log(1 + ' uzduotis');
+
+function tusciaFunkcija() {
+    return 'false'
+}
+console.log(tusciaFunkcija());
+
+console.log(2 + ' uzduotis');
+
+const skaicius1 = 9;
+const skaicius2 = 17;
+const skaicius3 = 33;
+
+function daugyba(a, b) {
+    const sandauga = a * b;
+    return sandauga;
+}
+
+console.log(daugyba(skaicius1, skaicius2));
+console.log(daugyba(skaicius3, skaicius2));
+console.log(daugyba(skaicius1, skaicius3));
+console.log(daugyba(2, 7));
